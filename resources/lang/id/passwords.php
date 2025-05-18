bca41bc9255092f5645ca6f2cac151e919fdb2f4
pesoros <pesorosdev@gmail.com>
2025-05-18 16:36:11 +0000

    started

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'password' => 'Kata sandi harus minimal delapan karakter dan cocok dengan konfirmasi.',
+    'reset' => 'Kata sandi Anda telah berhasil diatur ulang!',
+    'sent' => 'Kami telah mengirimkan tautan untuk atur ulang kata sandi ke email Anda!',
+    'token' => 'Token untuk atur ulang kata sandi tidak valid.',
+    'user' => 'Kami tidak menemukan pengguna dengan alamat email tersebut.',
+    'throttled' => 'Anda baru saja meminta pengaturan ulang kata sandi, silakan periksa email Anda.',
+];
